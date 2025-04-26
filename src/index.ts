2efbccdc9ecbb879c3ff584ac83029f597106fc0
Haovi Zhang <yukiniro@hotmail.com>
2025-04-26 23:27:53 +0800

    chore: 添加dotenv和@cloudflare/workers-types依赖，更新wrangler.jsonc和index.ts以支持新变量

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import { env } from 'hono/adapter'
 
-const app = new Hono()
+type Bindings = {
+  PROMPT: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.all('/prompt', (c) => {
+  const prompt = env(c).PROMPT
+  return c.text(prompt || "No prompt!")
 })
 
 export default app
